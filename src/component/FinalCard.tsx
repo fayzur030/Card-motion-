17beebcfc43b6e023f84ed769fc2b3d61a0a855e
Md. Fayzur Rahman <mdfayzurrahman030@gmail.com>
2025-11-20 15:14:17 +0600

    updated

--- src/component/FinalCard.tsx
+++ src/component/FinalCard.tsx
@@ -23,35 +23,35 @@ const cards = [
     title: 'Trade with clarity',
     description: 'Every rule exists to empower, not restrict',
     icon: <Antenna size={36} color='#F5A623' />,
-    rotate: -2,
+    rotate: -2.5,
   },
   {
     id: 3,
     title: 'Every parameter matters',
     description: 'Transparent, balanced, and fair from day one',
     icon: <SlidersHorizontal size={36} color='#F5A623' />,
-    rotate: 1,
+    rotate: 0,
   },
   {
     id: 4,
     title: 'Grow without limits',
     description: 'The better you perform, the more we scale with you',
     icon: <Sprout size={36} color='#F5A623' />,
-    rotate: -1,
+    rotate: -1.5,
   },
   {
     id: 5,
     title: 'Your focus on the charts',
     description: 'We handle everything else from funding to payouts',
     icon: <ChartNoAxesCombined size={36} color='#F5A623' />,
-    rotate: 1,
+    rotate: -3,
   },
   {
     id: 6,
     title: 'No distractions, No noise',
     description: 'Just pure performance',
     icon: <VolumeOff size={36} color='#F5A623' />,
-    rotate: -1,
+    rotate: -4,
   },
   {
     id: 7,
@@ -72,11 +72,18 @@ function Card({
   scrollYProgress: MotionValue<number>
 }) {
   const scale = useTransform(scrollYProgress, [0, 1], [0.95 + index * 0.01, 1])
-  const y = useTransform(scrollYProgress, [0, 1], [50, 0])
+
+  const y = useTransform(scrollYProgress, [0, 1], [index * 85, 0])
+
+  const opacity = useTransform(
+    scrollYProgress,
+    [0, 0.2 + index * 0.05, 0.4 + index * 0.05],
+    [0, 1, 1]
+  )
 
   return (
     <motion.div
-      style={{ scale, y, rotate: card.rotate }}
+      style={{ scale, y, opacity, rotate: card.rotate }}
       className={`sticky top-20 mb-6 text-white bg-gradient-to-tr from-black via-gray-900 to-amber-900/40 rounded-2xl shadow-lg p-6 border border-gray-500`}
     >
       <div className='mb-4'>{card.icon}</div>
@@ -95,7 +102,7 @@ export default function StackingStickyCards() {
 
   return (
     <section className='h-[300vh] flex items-start justify-center bg-black pt-20'>
-      <div className='w-full max-w-sm relative' ref={ref}>
+      <div className='w-full max-w-lg relative' ref={ref}>
         {cards.map((card, index) => (
           <Card
             key={card.id}
