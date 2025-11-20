e7b92b17cf00eb329689dc687462126e94a722fa
Md. Fayzur Rahman <mdfayzurrahman030@gmail.com>
2025-11-20 10:30:10 +0600

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import './App.css'
-// import Card12 from './component/Card'
-// import Cart from './component/Cart'
-// import Cards from './component/Exmple'
 import StackingStickyCards from './component/FinalCard'
 
 function App() {
   return (
     <>
-      <div className='bg-black'>
+      <div>
         <StackingStickyCards />
-        {/* <Cart />  */}
-        {/* <Cards/> */}
-        {/* <Card12/> */}
       </div>
     </>
   )
